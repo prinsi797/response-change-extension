c17e677226617244580a0f12259a8ef9c996342e
prinsi797 <148058521+prinsi797@users.noreply.github.com>
2024-12-28 15:09:49 +0530

    other website call a api response change

--- content.js
+++ content.js
@@ -7,6 +7,38 @@ function transformResponse(originalData) {
         data: Array.isArray(originalData) ? originalData : [originalData]
     };
 }
+
+const originalFetch = window.fetch;
+
+window.fetch = async function(...args) {
+    console.log('Intercepted fetch call:', args[0]);
+    
+    const response = await originalFetch(...args);
+    
+    if (args[0].includes('partitionwala.kryzetech.com/api/categories')) {
+        console.log('Intercepting categories API response');
+        
+        const clone = response.clone();
+        try {
+            const originalData = await clone.json();
+            const transformedData = transformResponse(originalData);
+            
+            console.log('Transformed response:', transformedData);
+            
+            return new Response(JSON.stringify(transformedData), {
+                status: response.status,
+                statusText: response.statusText,
+                headers: response.headers
+            });
+        } catch (error) {
+            console.error('Error transforming response:', error);
+            return response;
+        }
+    }
+    
+    return response;
+};
+
 const XHR = XMLHttpRequest.prototype;
 const open = XHR.open;
 const send = XHR.send;
@@ -16,7 +48,6 @@ XHR.open = function(method, url) {
     this._method = method;
     this._url = url;
     this._requestHeaders = {};
-    this._startTime = (new Date()).toISOString();
     return open.apply(this, arguments);
 };
 
@@ -27,43 +58,25 @@ XHR.setRequestHeader = function(header, value) {
 
 XHR.send = function(postData) {
     this.addEventListener('load', function() {
-        try {
-            const responseBody = this.response;
-            const originalData = JSON.parse(responseBody);
-            const transformedData = transformResponse(originalData);
-            
-            Object.defineProperty(this, 'response', {
-                value: JSON.stringify(transformedData)
-            });
-            Object.defineProperty(this, 'responseText', {
-                value: JSON.stringify(transformedData)
-            });
-            
-            console.log('Modified API Response:', transformedData);
-        } catch (err) {
-            console.error('Error modifying response:', err);
+        if (this._url.includes('partitionwala.kryzetech.com/api/categories')) {
+            try {
+                const originalData = JSON.parse(this.responseText);
+                const transformedData = transformResponse(originalData);
+                
+                Object.defineProperty(this, 'response', {
+                    value: JSON.stringify(transformedData)
+                });
+                Object.defineProperty(this, 'responseText', {
+                    value: JSON.stringify(transformedData)
+                });
+                
+                console.log('Modified XHR Response:', transformedData);
+            } catch (err) {
+                console.error('Error in XHR intercept:', err);
+            }
         }
     });
     return send.apply(this, arguments);
-};
-
-const originalFetch = window.fetch;
-window.fetch = async function(...args) {
-    const response = await originalFetch(...args);
-    
-    const clone = response.clone();
-    try {
-        const originalData = await clone.json();
-        const transformedData = transformResponse(originalData);
-        
-        return new Response(JSON.stringify(transformedData), {
-            status: response.status,
-            statusText: response.statusText,
-            headers: response.headers
-        });
-    } catch (error) {
-        return response;
-    }
 };
 
 console.log('Content script is running and API interceptor is active!');
